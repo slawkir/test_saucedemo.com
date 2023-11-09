ba56e6a7b38771d89481e0bbc882f2e5a3fbfb6b
slawkir <slawkir1984@gmail.com>
2023-11-09 14:00:17 +0400

    feat: add step 3 'go to cart'

--- tests/test_task1.py
+++ tests/test_task1.py
@@ -6,7 +6,9 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get('https://www.saucedemo.com/')
 
+
 # Шаг 1. Авторизация в приложении.
+
 user_name = driver.find_element(By.ID, 'user-name')
 user_name.send_keys("performance_glitch_user")
 
@@ -15,16 +17,28 @@ user_pass.send_keys('secret_sauce')
 
 login_btn = driver.find_element(By.ID, 'login-button')
 login_btn.click()
+
 print('Авторизация')
 
 
 # Шаг 2. Добавить товар Sauce Labs Fleece Jacket в корзину (нажать кнопку ADD TO CART)
-time.sleep(5) # чтобы визуально отследить добавление товара в корзину
-add_to_cart = driver.find_element(By.ID, 'add-to-cart-sauce-labs-fleece-jacket')
-add_to_cart.click()
+
+time.sleep(2) # чтобы визуально отследить добавление товара в корзину
+
+btn_add_to_cart = driver.find_element(By.ID, 'add-to-cart-sauce-labs-fleece-jacket')
+btn_add_to_cart.click()
 
 print('Добавление товара')
 
+
+# Шаг 3. Перейти в корзину нажатием кнопки сверху в правом углу в виде корзины
+
+time.sleep(2)
+
+shopping_cart = driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a')
+shopping_cart.click()
+
+print('Переход в корзину')
 
 #чтобы страница после запуска резко не закрывалась
 while(True):
