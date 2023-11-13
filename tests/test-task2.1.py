f4a950765e75dbf79611454c9e6529c3e8a60888
slawkir <slawkir1984@gmail.com>
2023-11-13 07:08:14 +0400

    fix: изменение алгоритма проверки сортировки цены

--- tests/test-task2.1.py
+++ tests/test-task2.1.py
@@ -1,13 +1,13 @@
 # сортировка «Low to high»
-
+import re
 import time
+
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get('https://www.saucedemo.com/')
-
 
 # Шаг 1. Авторизация в приложении.
 user_name = driver.find_element(By.ID, 'user-name')
@@ -19,8 +19,7 @@ login_btn.click()
 print('Авторизация')
 time.sleep(2)
 
-
-#Шаг 2. Нажать на выпадающий список
+# Шаг 2. Нажать на выпадающий список
 product_sort_container = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select')
 product_sort_container.click()
 print('Клик по выпадающему списку')
@@ -29,9 +28,26 @@ time.sleep(5)
 # Шаг 3. Клик по элементу "Price (low to high)"
 option_low_high = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select/option[3]')
 option_low_high.click()
-print('Клик по элементу "Price (low to high)"')
+
+product_prices_webelements = driver.find_elements(By.CLASS_NAME, 'inventory_item_price')
+product_prices_value_text = []
+product_prices_value_number = []
+
+for element in product_prices_webelements:
+    product_prices_value_text.append(element.text.replace('$', ''))
+
+print(product_prices_value_text)
+
+for element in product_prices_value_text:
+    product_prices_value_number.append(float(element))
+print(product_prices_value_number)
+
+product_prices_value_number_sort = sorted(product_prices_value_number)
+
+if product_prices_value_number_sort == product_prices_value_number:
+    print("Продукты отсортированы по возрастанию цены")
+else:
+    print("Ошибка сортировки")
 time.sleep(5)
-
-
-
+driver.close()
 
--- tests/test-task2.2.py
+++ tests/test-task2.2.py
@@ -23,11 +23,37 @@ time.sleep(2)
 #Шаг 2. Нажать на выпадающий список
 product_sort_container = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select')
 product_sort_container.click()
+
+
 print('Клик по выпадающему списку')
 time.sleep(5)
 
 # Шаг 3. Клик по элементу "Price (high to low)"
 option_high_low = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select/option[4]')
 option_high_low.click()
-print('Клик по элементу "Price (high to low)"')
+
+
+
+product_prices_webelements = driver.find_elements(By.CLASS_NAME, 'inventory_item_price')
+product_prices_value_text = []
+product_prices_value_number = []
+
+for element in product_prices_webelements:
+    product_prices_value_text.append(element.text.replace('$', ''))
+
+print(product_prices_value_text)
+
+for element in product_prices_value_text:
+    product_prices_value_number.append(float(element))
+print(product_prices_value_number)
+
+product_prices_value_number_sort = sorted(product_prices_value_number, reverse=True)
+
+if product_prices_value_number_sort == product_prices_value_number:
+    print("Продукты отсортированы по убыванию  цены")
+else:
+    print("Ошибка сортировки")
+
+
 time.sleep(5)
+driver.close()
